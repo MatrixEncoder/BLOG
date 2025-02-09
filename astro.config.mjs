01fbe3be12ad0c61ff25d07fb4783820dcfa1ec6
SURYANSH SRIVASTAVA <dhawalsri77@gmail.com>
2025-02-09 20:22:44 +0530

    Update Astro config and deployment workflow for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
+  site: 'https://matrixencoder.github.io',
+  base: '/BLOG',
   integrations: [tailwind()]
 });
